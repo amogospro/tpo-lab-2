3d07f74b955c16cf92f53659017bd9858221dd4c
Егор Деревягин <egord1010@mail.ru>
2025-04-02 15:47:41 +0700

    trigonometric

--- tpo-lab-2/build.gradle.kts
+++ tpo-lab-2/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("ch.obermuhlner:big-math:2.3.2")
 }
 
 tasks.test {
